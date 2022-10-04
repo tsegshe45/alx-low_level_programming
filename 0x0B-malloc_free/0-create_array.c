6e7f148bc846c112de676e06544a9766c600544a
tsegshe45 <tsegshe45@gmail.com>
2022-10-04 03:44:00 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,17 +12,17 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array;
-        unsigned int index;
+	unsigned int index;
 
-        if (size == 0)
-                return (NULL);
-        array = malloc(sizeof(char) * size);
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
 
-        if (array == NULL)
-                return (NULL);
+	if (array == NULL)
+		return (NULL);
 
-        for (index = 0; index < size; index++)
-                array[index] = c;
+	for (index = 0; index < size; index++)
+		array[index] = c;
 
-        return (array);
+	return (array);
 }
