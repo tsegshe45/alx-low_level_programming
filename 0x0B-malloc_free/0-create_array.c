41f4da56c7cf08a2057266f4cd5befc55c4d4de8
tsegshe45 <tsegshe45@gmail.com>
2022-10-05 00:03:06 -0700

    git commit -m '0-create_array.c'

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * create_array – creates an array of chars, and
- * initializes it with a specific char.
- *@size: Size of the array
- *@c: Character to insert
- *Return: Null if size is zero or if it fails.
- *pointer to array if everything is normal.
- */
+* create_array – creates an array of chars, and
+* initializes it with a specific char.
+*@size: Size of the array
+*@c: Character to insert
+*Return: Null if size is zero or if it fails.
+*pointer to array if everything is normal.
+*/
 
 char *create_array(unsigned int size, char c)
 {
